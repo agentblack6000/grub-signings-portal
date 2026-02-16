d1cf41a650031ca6b276a6faac3dc73a6d866f23
agentblack6000 <halan.raghav@gmail.com>
2026-02-17 03:19:13 +0530

    Setup bare implementation of JWT and Django management commands to add users

---
+++ grubsigningsportal/signings/management/commands/populate_users.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    # TODO: Create some base users and load them based on the model implemented for students
+
+    def handle(self, *args, **options):
+        pass
+    
