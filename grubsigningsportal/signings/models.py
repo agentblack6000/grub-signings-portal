7b9d590f7558ae1b8ab0c4516ef989e8ff2b0bd9
agentblack6000 <halan.raghav@gmail.com>
2026-02-18 16:26:53 +0530

    Update fields related to money to use DecimalField() instead of integers

--- grubsigningsportal/signings/models.py
+++ grubsigningsportal/signings/models.py
@@ -1,32 +1,36 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# TODO: review
 
-class CustomUser(AbstractUser):
-    dues = models.PositiveIntegerField(default=0) # usually grub prices are a whole number
+# TODO: Review changes
+
+class Student(AbstractUser):
+    dues = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     def __str__(self):
         return self.username
 
 class Grub(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    price_veg = models.PositiveIntegerField(null=True, blank=True)
-    price_nonveg = models.PositiveIntegerField(null=True, blank=True)
+    price_veg = models.DecimalField(null=True, default=0, max_digits=8, decimal_places=2)
+    price_nonveg = models.DecimalField(null=True, default=0, max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.name
     
 class Ticket(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="tickets")
     grub = models.ForeignKey(Grub, on_delete=models.CASCADE)
+
     class Type(models.TextChoices):
         VEG = 'VEG', 'Vegetarian'
         NONVEG = 'NONVEG', 'Non-Vegetarian'
+
     class Status(models.TextChoices):
         ACTIVE = 'ACTIVE', 'Active'
         CANCELLED = 'CANCELLED', 'Cancelled'
         USED = 'USED', 'Used'
+
     type = models.CharField(max_length=10, choices=Type.choices)
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.ACTIVE)
 
@@ -35,12 +39,14 @@ class Ticket(models.Model):
 
 class Transaction(models.Model):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
     class Type(models.TextChoices):
         PAYMENT = 'PAYMENT', 'Payment'
         REFUND = 'REFUND', 'Refund'
+
     type = models.CharField(max_length=10, choices=Type.choices, default=Type.PAYMENT)
     timestamp = models.DateTimeField(auto_now_add=True)
-    amount = models.IntegerField()
-    # figure out amount in views instead of auto assigning; couldn't think if a simpler implementation for this
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+
     def __str__(self):
         return (f"{self.ticket.user.username} - {self.ticket.grub.name} - {self.amount}")
