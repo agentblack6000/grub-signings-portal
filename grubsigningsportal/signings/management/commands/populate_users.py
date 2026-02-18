448c118c1d9335793be748d278e718003190d93d
Abhiraj <abhirajpharate@gmail.com>
2026-02-18 19:31:33 +0530

    added dummy user data

--- grubsigningsportal/signings/management/commands/populate_users.py
+++ grubsigningsportal/signings/management/commands/populate_users.py
@@ -2,7 +2,8 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     # TODO: Create some base users and load them based on the model implemented for students
-
+    # TODO: use user.objects.create_user() instead of just create() so password is hashed automatically
+    
     def handle(self, *args, **options):
         pass
     
